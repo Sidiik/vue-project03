ebc520beebaa47aed414d5b34865fa47aa84765b
Sadiq O <sadiqomar890@gmail.com>
2021-08-23 19:37:21 +0300

    modified form

--- src/components/SignUP.vue
+++ src/components/SignUP.vue
@@ -9,8 +9,36 @@
       class="form-control"
       v-model="password"
     />
+    <label for="role" id="">Role</label>
+    <select class="form-select" v-model="role">
+      <option value="designer">Designer</option>
+      <option value="developer">Developer</option>
+    </select>
+    <div class="check">
+      <input type="checkbox" class="form-check-input " v-model="terms" /> Accept
+      terms and conditions
+    </div>
+
+    <div class="skills">
+      <label for="">SKILLS (alt + , )</label>
+      <input
+        type="text"
+        class="form-control"
+        v-model="tempSkills"
+        @keyup.alt="addSkill"
+      />
+      <div
+        v-for="skill in skills"
+        class="badge bg-secondary p-2 m-1"
+        :key="skill"
+      >
+        {{ skill }}
+      </div>
+    </div>
+
     <button class="btn btn-primary rounded mt-3">Create account</button>
   </form>
+  <p>{{ email }}</p>
 </template>
 
 <script>
@@ -20,7 +48,19 @@ export default {
     return {
       password: "",
       email: "",
+      role: "",
+      terms: false,
+      tempSkills: [],
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkills) {
+        this.skills.push(this.tempSkills);
+        this.tempSkills = "";
+      }
+    },
   },
 };
 </script>
@@ -28,11 +68,20 @@ export default {
 <style>
 form {
   width: 40%;
-  margin: auto;
   background: #fff;
   padding: 2rem;
   border-radius: 0.6rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
     rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  text-align: left;
+  margin: auto;
+}
+
+label,
+.check {
+  margin-top: 1rem;
+}
+input[type="checkbox"] {
+  margin-right: 0.5rem;
 }
 </style>
