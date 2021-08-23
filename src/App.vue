42afc69a760e47f9a3e075501b6184c085f53832
Sadiq O <sadiqomar890@gmail.com>
2021-08-23 18:20:13 +0300

    made changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Sign up for the interview</h2>
+  <SignUp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from "./components/SignUP.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { SignUp },
+};
 </script>
 
 <style>
@@ -22,5 +20,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #eee;
 }
 </style>
